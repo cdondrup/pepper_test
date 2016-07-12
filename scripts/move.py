fc19bf98a41fa30979bbc579d0384b61810b924e
Christian Dondrup <cdondrup@gmail.com>
2016-07-12 10:19:09 +0100

    Allowing to move sideways

--- scripts/move.py
+++ scripts/move.py
@@ -48,16 +48,17 @@ class _GetchWindows:
 
 
 class KeyBoardTeleOp(object):
+    __kill_key = 'k'
     def __init__(self, robot_ip, port):
         self.robot_ip = robot_ip
         self.port = port
         self.connect(robot_ip, port)
         self.vels = {
-            'x': {'current': .0, 'min': -.55, 'max': .55}, 
-            'y': {'current': .0, 'min': .0, 'max': .0}, 
+            'x': {'current': .0, 'min': -.55, 'max': .55},
+            'y': {'current': .0, 'min': -.55, 'max': .55},
             't': {'current': .0, 'min': -1., 'max': 1.}
         }
-        
+
     def connect(self, robot_ip, port):
         try:
             self.motion_proxy  = ALProxy("ALMotion", robot_ip, port)
@@ -67,7 +68,7 @@ class KeyBoardTeleOp(object):
             print "Cannot connect to %s:%s. Retrying in 1 second." % (robot_ip, str(port))
             time.sleep(1)
             self.connect(robot_ip, port)
-            
+
     def spin(self):
         getch = _Getch()
         ch = None
@@ -76,7 +77,7 @@ class KeyBoardTeleOp(object):
         self.tracking_mode = self.basic_awareness_proxy.getTrackingMode()
         self.basic_awareness_proxy.stopAwareness()
         print "Waiting for key"
-        while ch != 'q':
+        while ch != self.__kill_key:
             ch = getch()
             if ch == 'w':
                 if self.vels['x']['current'] + 0.05 < self.vels['x']['max']:
@@ -88,6 +89,16 @@ class KeyBoardTeleOp(object):
                     self.vels['x']['current'] -= 0.05
                 else:
                     self.vels['x']['current'] = self.vels['x']['min']
+            elif ch == 'q':
+                if self.vels['y']['current'] + 0.05 < self.vels['y']['max']:
+                    self.vels['y']['current'] += 0.05
+                else:
+                    self.vels['y']['current'] = self.vels['y']['max']
+            elif ch == 'e':
+                if self.vels['y']['current'] - 0.05 > self.vels['y']['min']:
+                    self.vels['y']['current'] -= 0.05
+                else:
+                    self.vels['y']['current'] = self.vels['y']['min']
             elif ch == 'a':
                 if self.vels['t']['current'] + 0.1 < self.vels['t']['max']:
                     self.vels['t']['current'] += 0.1
@@ -100,29 +111,30 @@ class KeyBoardTeleOp(object):
                     self.vels['t']['current'] = self.vels['t']['min']
             elif ch == ' ':
                 self.vels['x']['current'] = 0.0
+                self.vels['y']['current'] = 0.0
                 self.vels['t']['current'] = 0.0
 
             command = {}
             for k,v in self.vels.items():
                 command[k] = round(v['current'],2)
             self._move(**command)
-            if ch != 'q': print command
+            if ch != self.__kill_key: print command
 
         print "Starting basic awareness with %s and %s" % (self.awareness_mode, self.tracking_mode)
         self.motion_proxy.wakeUp()
         self.basic_awareness_proxy.setEngagementMode(self.awareness_mode)
         self.basic_awareness_proxy.setTrackingMode(self.tracking_mode)
-        self.basic_awareness_proxy.startAwareness()        
-        
+        self.basic_awareness_proxy.startAwareness()
+
         print "Good-bye"
         self._stop()
-            
+
     def _stop(self):
         self._move()
-        
+
     def _move(self, x=.0, y=.0, t=.0):
         self.motion_proxy.post.move(x, y, t)
-        
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="192.168.1.37",
@@ -130,6 +142,6 @@ if __name__ == "__main__":
     parser.add_argument("--port", type=int, default=9559,
                         help="Robot port number")
     args = parser.parse_args()
-    
+
     k = KeyBoardTeleOp(args.ip, args.port)
     k.spin()
